cdd27c67802e4deaec3b4176a24d5a50dc0b8e76
Ben Kaplan <bjminkaplan@gmail.com>
2022-10-03 14:42:35 -0500

    Fixing movement to one space at a time

--- script.js
+++ script.js
@@ -3,33 +3,52 @@ console.log('Hi')
 let spaces = document.querySelectorAll('td')
 let blackPieces = document.querySelectorAll('.blackPiece')
 let redPieces = document.querySelectorAll('.redPiece')
+let player = 'red'
 
 console.log(spaces)
 console.log(blackPieces)
 console.log(redPieces)
 
-const movement = () => {
-  blackPieces.forEach((piece) =>
-    piece.addEventListener('click', () => {
-      spaces.forEach((space) => {
-        space.addEventListener('click', () => {
-          space.appendChild(piece)
+const movement = (player) => {
+  let move = 0
+  if (player === 'red') {
+    redPieces.forEach((piece) =>
+      piece.addEventListener('click', () => {
+        spaces.forEach((space) => {
+          space.addEventListener('click', () => {
+            space.appendChild(piece)
+            move++
+            console.log(move)
+          })
         })
       })
-    })
-  )
+    )
+  } else {
+    blackPieces.forEach((piece) =>
+      piece.addEventListener('click', () => {
+        spaces.forEach((space) => {
+          space.addEventListener('click', () => {
+            space.appendChild(piece)
+            move++
+            console.log(move)
+          })
+        })
+      })
+    )
+  }
+  playerSwitch()
 }
 
-movement()
-//click on piece
-//then click on space to move to
-//remove class of checker piece then add it to the space clicked on
+const playerSwitch = (player) => {
+  if (player === 'black') {
+    player = 'red'
+  } else {
+    player = 'black'
+  }
+}
+
+movement(player)
 
 const playGame = () => {}
 
 //Event Listeners
-
-// document.getElementById('.blackPiece').addEventListener('click', movement)
-// document.getElementById('.redPiece').addEventListener('click', playGame)
-
-// document.getElementById('.td').addEventListener('click', () => {})
