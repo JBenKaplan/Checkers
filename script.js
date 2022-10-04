c6c7ea3237e0d46c02d750a792836db198f38b6b
Ben Kaplan <bjminkaplan@gmail.com>
2022-10-04 12:43:29 -0500

    Testing removing pointerEvents

--- script.js
+++ script.js
@@ -1,55 +1,93 @@
-console.log('Hi')
+const board = [
+  null,
+  1,
+  null,
+  2,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  3,
+  null,
+  4,
+  null
+]
 
 let spaces = document.querySelectorAll('td')
 let blackPieces = document.querySelectorAll('.blackPiece')
 let redPieces = document.querySelectorAll('.redPiece')
-let player = redPieces
+
+let currentPlayer = redPieces
+
+let scoreRed = 0
+let scoreBlack = 0
 
 console.log(spaces)
 console.log(blackPieces)
 console.log(redPieces)
 
-const movement = (player) => {
-  player.forEach((piece) =>
-    piece.addEventListener('click', () => {
-      spaces.forEach((space) => {
-        space.addEventListener('click', () => {
-          space.appendChild(piece)
-          piece.removeEventListener('click', movement)
-          space.removeEventListener('click', movement)
-        })
-      })
-    })
-  )
+let playPiece = {
+  pieceID: -1,
+  boardSpace: -1,
+  moveThird: false,
+  moveFifth: false,
+  moveNegThird: false,
+  moveNegFifth: false
 }
-// Other method for moving pieces?
-// if ((player = 'red')) {
-//   for (let i = 0; i < redPieces.length; i++) {
-//     redPieces[i].addEventListener('click', () => {
-//       for (let i = 0; i < spaces.length; i++) {
-//         spaces.appendChild(redPieces[i])
-//       }
-//     })
-//   }
-// } else {
-//   for (let i = 0; i < blackPieces.length; i++) {
-//     blackPieces[i].addEventListener('click', () => {
-//       spaces.appendChild(blackPieces[i])
-//     })
-//   }
-// }
 
-const playerSwitch = (player) => {
-  if (player === 'black') {
-    player = 'red'
+let resetPiece = {
+  pieceID: -1,
+  boardSpace: -1,
+  moveThird: false,
+  moveFifth: false,
+  moveNegThird: false,
+  moveNegFifth: false
+}
+
+const playerSwitch = () => {
+  if (currentPlayer === redPieces) {
+    //disable cursor on black tiles
+    //remove red piece movement -> future: except for movable
+    currentPlayer = blackPieces
+    console.log('black')
+    document.getElementsByTagName('p').style.pointerEvents = 'none'
+    document.getElementsByTagName('td').style.pointerEvents = 'none'
+    document.getElementsByTagName('p').style.cursor = 'auto'
+    // movePiece(blackPieces)
   } else {
-    player = 'black'
+    //disable cursor on black tiles
+    //remove black piece movement -> future: except for movable
+    currentPlayer = redPieces
+    console.log('red')
+    document.getElementsByTagName('div').style.pointerEvents = 'none'
+    document.getElementsByTagName('td').style.pointerEvents = 'none'
+    document.getElementsByTagName('div').style.cursor = 'auto'
+    // movePiece(redPieces)
   }
 }
 
-movement(player)
+const addClicks = () => {
+  if (currentPlayer === redPieces) {
+    for (let i = 0; i < redPieces.length; i++) {
+      redPieces[i].addEventListener('click', findPiece)
+    }
+  } else {
+    for (let i = 0; i < redPieces.length; i++) {
+      redPieces[i].addEventListener('click', findPiece)
+    }
+  }
+  movePiece()
+}
 
-const playGame = () => {}
+let findPiece = () => {
+  let num = parseInt(playPiece.pieceID)
+  return board.indexOf(num)
+}
 
-//Event Listeners
-// player.addEventListener('click', playGame)
+const movePiece = () => {}
+
+addClicks()
